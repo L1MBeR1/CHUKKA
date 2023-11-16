41063ad080c904c121f5e7a4df2cf1dd0f831b55
L1MBeR1 <117737178+L1MBeR1@users.noreply.github.com>
2023-11-16 16:31:09 +0000

    .

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 // const db = require('../models/db');
 
 router.use(express.static(path.join(__dirname, '../public')));
