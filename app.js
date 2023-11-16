0304ea2e0090068cdcf289c27c836e06008d2d11
L1MBeR1 <117737178+L1MBeR1@users.noreply.github.com>
2023-11-16 15:35:32 +0000

    .

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
-const express = require("express");
-const path = require("path");
+
+const express = require('express');
+const path = require('path');
+// const db = require('./models/db');  // Предполагается, что ваш файл db.js находится в директории models
+
 const app = express();
 const port = process.env.PORT || 3001;
 
-const homeRouter = require('./routes/home');
+// Указываем Express использовать директорию 'views' для представлений
+app.set('views', path.join(__dirname, 'views'));
+
+// Указываем Express использовать шаблонизатор EJS
+app.set('view engine', 'ejs');
+
+// Маршруты
+const homeRoute = require('./routes/home');
+app.use('/', homeRoute);
+
+// Запуск сервера
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// const express = require("express");
+// const path = require("path");
+// const app = express();
+// const port = process.env.PORT || 3001;
+
+// const homeRouter = require('./routes/home');
 
 
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', homeRouter);
+// app.use('/', homeRouter);
 
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+// server.keepAliveTimeout = 120 * 1000;
+// server.headersTimeout = 120 * 1000;
