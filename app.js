19e09a875ba842a4747cbe97821db0e03132a66b
L1MBeR1 <117737178+L1MBeR1@users.noreply.github.com>
2023-11-16 15:13:12 +0000

    .

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3001;
 
+const homeRouter = require('./routes/home');
 
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use('/', homeRouter);
-app.use('/catalog', catalogRouter);
+
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
