d1df1cefc9a67397d26e06a718dd418760a8e233
L1MBeR1 <117737178+L1MBeR1@users.noreply.github.com>
2023-11-16 16:13:20 +0000

    .

--- routes/register.js
+++ routes/register.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
-const db = require('./bd'); // Путь к вашему файлу bd.js
+// const db = require('./bd'); // Путь к вашему файлу bd.js
 
 // Используем middleware bodyParser.urlencoded для обработки данных из формы
 router.use(bodyParser.urlencoded({ extended: true }));
