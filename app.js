daacd14583a4183cdde6f5e4102f665bc9891916
L1MBeR1 <117737178+L1MBeR1@users.noreply.github.com>
2023-11-16 16:05:16 +0000

    Bd update

--- app.js
+++ app.js
@@ -12,29 +12,15 @@ app.set('views', path.join(__dirname, 'views'));
 // Указываем Express использовать шаблонизатор EJS
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'public')));
 // Маршруты
 const homeRoute = require('./routes/home');
-app.use('/', homeRoute);
+const registerRoute = require('./register');
 
+app.use('/', homeRoute);
+app.use('/register', registerRoute);
 // Запуск сервера
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-// const express = require("express");
-// const path = require("path");
-// const app = express();
-// const port = process.env.PORT || 3001;
-
-// const homeRouter = require('./routes/home');
-
-
-// app.use(express.static(path.join(__dirname, "public")));
-
-// app.use('/', homeRouter);
-
-
-// const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-// server.keepAliveTimeout = 120 * 1000;
-// server.headersTimeout = 120 * 1000;
---
+++ routes/register.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const db = require('./bd'); // Путь к вашему файлу bd.js
+
+// Используем middleware bodyParser.urlencoded для обработки данных из формы
+router.use(bodyParser.urlencoded({ extended: true }));
+
+// Роут для страницы регистрации (GET-запрос)
+router.get('/', (req, res) => {
+  res.render('register');
+});
